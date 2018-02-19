baedcc910dcc3ccfb1c9ae20ab969b411be315c4
Anish Khattar <prgrmer25@gmail.com>
2018-02-18 17:04:03 -0800

    adding a star baord

---
+++ EdX/ArtificialIntelligence/SearchAlgorithms/astar_board.py
@@ -0,0 +1,18 @@
+from board import Board
+
+class AStarBoard(Board):
+	def __init__(self, board_state, moves, depth, cost, board_size):
+		Board.__init__(self, board_state, moves, depth, cost, board_size)
+
+	def get_total_manhattan_dist(self):
+		total_dist = 0
+		for i in range(0, len(self._board_state)):
+			curr_row, curr_col = self._get_row_col(i)
+			des_row, des_col = self._get_row_col(self._board_state[i])
+			total_dist += abs(des_row - curr_row) + abs(des_col - curr_col)
+		return total_dist
+
+	def _get_new_board(self, board_state, moves, depth, cost, board_size):
+		return AStarBoard(board_state, moves, depth, cost, board_size)
+
+	

c06b3a02727af49e859db55487079d74b31f7fe3
Anish Khattar <prgrmer25@gmail.com>
2018-02-18 18:22:28 -0800

    making data structures private

--- EdX/ArtificialIntelligence/SearchAlgorithms/bfs_fringe.py
+++ EdX/ArtificialIntelligence/SearchAlgorithms/bfs_fringe.py
@@ -4,18 +4,18 @@ from collections import deque
 
 class BFSFringe(Fringe):
 	def __init__(self):
-		self.queue = deque([])
+		self.__queue = deque([])
 
 	def add_boards(self, boards):
 		# boards are in U,D,L,R order. Push elements to queue in order
 		for b in boards:
-			self.queue.appendleft(b)
+			self.__queue.appendleft(b)
 
 	def get_next_board(self):
-		if (len(self.queue) > 0):
-			return self.queue.pop()
+		if (len(self.__queue) > 0):
+			return self.__queue.pop()
 		else:
 			raise Exception("no more elements left in BFS fringe")
 
 	def get_size(self):
-		return len(self.queue)
+		return len(self.__queue)
--- EdX/ArtificialIntelligence/SearchAlgorithms/dfs_fringe.py
+++ EdX/ArtificialIntelligence/SearchAlgorithms/dfs_fringe.py
@@ -3,18 +3,18 @@ from fringe import Fringe
 class DFSFringe(Fringe):
 
 	def __init__(self):
-		self.stack = []
+		self.__stack = []
 
 	def add_boards(self, boards):
 		# boards are in U,D,L,R order. Push elements to stack in reverse order
 		for i in range(len(boards) - 1, -1, -1):
-			self.stack.append(boards[i])
+			self.__stack.append(boards[i])
 
 	def get_next_board(self):
-		if (len(self.stack) > 0):
-			return self.stack.pop()
+		if (len(self.__stack) > 0):
+			return self.__stack.pop()
 		else:
 			raise Exception("no more elements left in DFS fringe")
 
 	def get_size(self):
-		return len(self.stack)
+		return len(self.__stack)
